e08faa66e9dbd589b3c6ec29b489e47e99db535c
saeidpiryaee <piryaeesaeid@gmail.com>
2023-08-11 11:06:56 +0330

    fixes invisibl button problem

--- app/src/main/java/com/KiowSoft/InsearchOfSnowy/MainActivity.kt
+++ app/src/main/java/com/KiowSoft/InsearchOfSnowy/MainActivity.kt
@@ -2,67 +2,49 @@ package com.KiowSoft.InsearchOfSnowy
 
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.appodeal.ads.Appodeal
 import com.appodeal.ads.initializing.ApdInitializationCallback
 import com.appodeal.ads.initializing.ApdInitializationError
-import java.lang.Boolean
-import kotlin.Int
-
-
-
-
-
 
 class MainActivity : AppCompatActivity() {
 
     private var currentScore = 0
     private lateinit var scoreTextView: TextView
+    private lateinit var button1: Button
+    private lateinit var button2: Button
+    private lateinit var button3: Button
+    private lateinit var centerButton: Button
     private val PREFS_NAME = "MyPrefs"
     private val SCORE_KEY = "score"
-    val DEBUG = Boolean.parseBoolean("true")
-    val APPLICATION_ID = "com.appodealstack.demo"
-    val BUILD_TYPE = "debug"
-    val VERSION_CODE = 1
-    val VERSION_NAME = "1.0"
-
-    // Field from default config.
-    val APP_KEY = "8133866ba0105a2ce930ac403a35c074d95e8dcf2d33c12c"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
 
         Appodeal.initialize(this, "8133866ba0105a2ce930ac403a35c074d95e8dcf2d33c12c", Appodeal.REWARDED_VIDEO,
             object : ApdInitializationCallback {
                 override fun onInitializationFinished(errors: List<ApdInitializationError>?) {
-
+                    // Handle initialization finished
                 }
-            }
-            )
-
-
-
-
-
-
-        setContentView(R.layout.activity_main)
+            })
 
         scoreTextView = findViewById(R.id.scoreTextView)
-        val button1: Button = findViewById(R.id.button1)
-        val button2: Button = findViewById(R.id.button2)
-        val button3: Button = findViewById(R.id.button3)
-        val centerButton: Button = findViewById(R.id.centerButton)
+        button1 = findViewById(R.id.button1)
+        button2 = findViewById(R.id.button2)
+        button3 = findViewById(R.id.button3)
+        centerButton = findViewById(R.id.centerButton)
 
         // Restore the saved score
         val prefs: SharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
         currentScore = prefs.getInt(SCORE_KEY, 0)
         updateScore()
 
-        // Set click listeners for buttons (you can add functionality to these buttons if needed)
         button1.setOnClickListener {
-
             if (Appodeal.isLoaded(Appodeal.REWARDED_VIDEO)) {
                 // Show the rewarded video ad
                 Appodeal.show(this, Appodeal.REWARDED_VIDEO)
@@ -71,8 +53,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
         button2.setOnClickListener {
-
-            // Check if the rewarded video ad is loaded
             if (Appodeal.isLoaded(Appodeal.REWARDED_VIDEO)) {
                 // Show the rewarded video ad
                 Appodeal.show(this, Appodeal.REWARDED_VIDEO)
@@ -80,9 +60,12 @@ class MainActivity : AppCompatActivity() {
                 // Handle the case when the ad is not loaded
             }
         }
-        button3.setOnClickListener { }
-        centerButton.setOnClickListener { }
+        button3.setOnClickListener { /* Handle button3 click */ }
+        centerButton.setOnClickListener { /* Handle centerButton click */ }
 
+
+        checkAdLoadedStatus()
+        //button1.visibility = View.INVISIBLE
     }
 
     override fun onStop() {
@@ -96,6 +79,22 @@ class MainActivity : AppCompatActivity() {
     private fun updateScore() {
         scoreTextView.text = "Score: $currentScore"
     }
+
+    private fun checkAdLoadedStatus() {
+        val isAdLoaded = Appodeal.isLoaded(Appodeal.REWARDED_VIDEO)
+
+        if (isAdLoaded) {
+            // Ad is loaded, update UI or perform actions
+             button1.visibility = View.VISIBLE
+        } else {
+            // Ad is not loaded, update UI or perform actions
+           // button1.visibility = View.INVISIBLE
+        }
+
+        // Schedule the next check (e.g., after a delay or using a handler)
+        // You can use a handler, coroutine, or other mechanisms for scheduling checks.
+    }
+
 
     // You can call this method whenever you want to increase the score
     private fun increaseScore(points: Int) {
